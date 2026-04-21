0537128c363e8248d95d594ba0c2c3ddb8e0ee2a
EugeniaKolodeichuk <kolodeichuk.e@gmail.com>
2026-04-22 00:09:38 +0300

    + model switcher

--- src/components/three/ModelSwitcher.jsx
+++ src/components/three/ModelSwitcher.jsx
@@ -1,8 +1,12 @@
 import { PresentationControls } from "@react-three/drei";
 import { useRef } from "react"
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import MacbookModel16 from "../models/Macbook-16";
 import MacbookModel14 from "../models/Macbook-14";
 
+const SCALE_LARGE_DESKTOP = 0.08;
+const SCALE_LARGE_MOBILE = 0.05;
 const ANIMATION_DURATION = 1;
 const OFFSET_DISTANCE = 5;
 
@@ -27,7 +31,23 @@ const ModelSwitcher = ({ scale, isMobile }) => {
     const smallMacbookRef = useRef();
     const largeMacbookRef = useRef();
 
-    const showLargeMacbook = scale === 0.08 || scale === 0.05;
+    const showLargeMacbook = scale === SCALE_LARGE_DESKTOP || scale === SCALE_LARGE_MOBILE;
+
+    useGSAP(() => {
+        if (showLargeMacbook) {
+            moveGroup(smallMacbookRef.current, -OFFSET_DISTANCE);
+            moveGroup(largeMacbookRef.current, 0);
+
+            fadeMeshes(smallMacbookRef.current, 0);
+            fadeMeshes(largeMacbookRef.current, 1);
+        } else {
+            moveGroup(smallMacbookRef.current, 0);
+            moveGroup(largeMacbookRef.current, OFFSET_DISTANCE);
+
+            fadeMeshes(smallMacbookRef.current, 1);
+            fadeMeshes(largeMacbookRef.current, 0);
+        }
+    }, [scale]);
 
     const controlsConfig = {
         snap: true,
@@ -41,7 +61,7 @@ const ModelSwitcher = ({ scale, isMobile }) => {
         <>
             <PresentationControls {...controlsConfig}>
                 <group ref={largeMacbookRef}>
-                    <MacbookModel16 scale={isMobile ? 0.05 : 0.08} />
+                    <MacbookModel16 scale={isMobile ? SCALE_LARGE_MOBILE : SCALE_LARGE_DESKTOP} />
                 </group>
             </PresentationControls>
             <PresentationControls {...controlsConfig}>
